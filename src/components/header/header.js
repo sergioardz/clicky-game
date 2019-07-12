d55b10f1e85ca4aaada77a0a205b978872372b72
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-07-12 11:34:27 -0500

    Included Images, Improved Layout, CSS and Responsiveness

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ import "./header.css";
 
 const Header = props => (
     <header className="header">
-        <h1>Clicky Game!</h1>
+        <h1>Let's try your memory!</h1>
         <h2>Click on an image to earn points, but don't click on any more than once!</h2>
     </header>
 );
